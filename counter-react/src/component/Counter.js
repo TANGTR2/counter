a606065573cfcef9150fc95fcdebb7cc40de2217
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-21 17:54:22 +0800

    rewrite finish counter component

---
+++ counter-react/src/component/Counter.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Counter extends Component{
+    constructor(props){
+        super(props);
+        this.state = {count:0}
+    }
+
+    increase =() =>{
+        this.setState({count:this.state.count +1})
+    }
+
+    decrease =() =>{
+        this.setState({count:this.state.count -1})
+    }
+
+    render(){
+        return (
+            <div>
+               <button onClick = {this.increase}> + </button>
+               <button onClick = {this.decrease}> - </button>
+               <span> count: {this.state.count}</span>
+           </div>
+        )  
+    }
+}
